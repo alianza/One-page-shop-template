d9bcecb76b6463db74182be451eed9c492cb8821
PhyrexAlianza <janwillemvanbremen@live.nl>
2017-05-16 23:26:56 +0200

    Finished databse functions and tested they work

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <!doctype html>
 
-<?php
-
-include("dbconfig.php");
-
-?>
-
 <html>
 	<head>
+        <?php include("dbconfig.php") ?>
 		<title>Onepage</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="initial-scale=1.0">
@@ -15,21 +10,25 @@ include("dbconfig.php");
 	</head>
 
 	<body>
+
+        <div id="page">
         <!--   Big image of product with text     -->
         <div id="top_div">
-            <?php echo $header_image ?>
+            <?php echo($header_image); ?>
         </div>
 
         <!--   Information about the 3 main features of product     -->
+        <div id="info_div_container">
+            <div class="v-align-helper"></div>
         <div id="info_div">
             <div id="info_div1">
                 <img id="info_div_img1" src="http://placehold.it/300x150">
-                <p><?php echo $title ?></p>
+                <p><?php echo($title); ?></p>
             </div>
 
             <div id="info_div2">
-                <img id="info_div_img3" src="http://placehold.it/300x150">
-                <p><?php echo $small_description ?></p>
+                <img id="info_div_img2" src="http://placehold.it/300x150">
+                <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
             </div>
 
             <div id="info_div3">
@@ -37,6 +36,8 @@ include("dbconfig.php");
                 <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
             </div>
         </div>
+        </div>
+
         <!--    information about delivery: order > deliver > time/wait    -->
         <div id="order_div">
             <div id="order_div1">
@@ -59,6 +60,6 @@ include("dbconfig.php");
         <div id="footer_div">
             <div id="footer_div1"></div>
         </div>
-
+        </div>
 	</body>
 </html>
