d02c59e82d631cb869192ce2af66cdd30ff5adf3
PhyrexAlianza <janwillemvanbremen@live.nl>
2017-05-16 21:28:17 +0200

    initial commit, database connect

---
+++ dbconfig.php
@@ -0,0 +1,32 @@
+<?php
+
+DEFINE("DB_USER", "root");
+DEFINE("DB_PASS", "");
+
+try {
+
+    $db = new PDO("mysql:host=localhost;dbname=onepage",DB_USER,DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    echo $e->getMessage();
+
+}
+
+function loginbarrier () {
+
+     echo("<div id='melding'>U moet ingelogd zijn om deze pagina weer te geven. Log <a href='index.php?page=login'>Hier</a> in!</div>");
+
+//    echo('<script>loginbarrier();</script>');
+
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+
+?>
