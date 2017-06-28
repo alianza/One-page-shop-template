8fcac6c95df21aeab78767c6c59e15c0d21f1b46
PhyrexAlianza <janwillemvanbremen@live.nl>
2017-06-28 18:26:23 +0200

    Defined all text and image variables

--- dbconfig.php
+++ dbconfig.php
@@ -26,33 +26,47 @@ try {
             $stmt->execute();
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+            $title = $result['title'];
+            $desc_text = $result['desc_text'];
+            $order_button = $result['order_button'];
+            $top_small_title = $result['top_small_title'];
+            $large_desc_1 = $result['large_desc_1'];
+            $large_desc_2 = $result['large_desc_2'];
+            $large_desc_3 = $result['large_desc_3'];
+            $buttom_small_title = $result['buttom_small_title'];
+            $small_desc_1 = $result['small_desc_1'];
+            $small_desc_2 = $result['small_desc_2'];
+            $small_desc_3 = $result['small_desc_3'];
+
      } catch(PDOException $e) {
 
             echo $e->GetMessage();
 
         }
-
-        $title = $result['title'];
-        $small_description = $result['desc_text'];
-        $large_description = $result['order_button'];
 
 //get Images
 
 try {
 
-            $sql = "SELECT * FROM `images` WHERE id = 1";
+            $sql = "SELECT * FROM `images` WHERE id = $template_ID";
 
             $stmt = $db->prepare($sql);
             $stmt->execute();
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+  //        $header_image = "data:image/jpeg;base64," . base64_encode( $result['header_image'] );
+            $header_image = base64_encode($result['header_image']);
+            $image_1 = base64_encode($result['image_1']);
+            $image_2 = base64_encode($result['image_2']);
+            $image_3 = base64_encode($result['image_3']);
+            $misc_image_1 = base64_encode($result['misc_image_1']);
+            $misc_image_2 = base64_encode($result['misc_image_2']);
+            $misc_image_3 = base64_encode($result['misc_image_3']);
+
      } catch(PDOException $e) {
 
             echo $e->GetMessage();
 
-        }
-
-//        $header_image = "data:image/jpeg;base64," . base64_encode( $result['header_image'] );
-        $header_image = base64_encode($result['header_image']);
+    }
 
 ?>
