451159e839ff2d27be64a5c60dfa0dfd980a696e
PhyrexAlianza <janwillemvanbremen@live.nl>
2017-07-22 13:37:12 +0200

    Updated PHP script to previous newest version

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 
 <html>
 	<head>
-        <?php include('DBconfig.php') ?>
 		<title>Onepage</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href=".//css/style.css">
-
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-
+        <?php include("dbconfig.php") ?>
 	</head>
 
 	<body>
@@ -17,108 +15,64 @@
         <div id="page">
         <!--   Big image of product with text     -->
         <div id="top_div">
-            <div id="top_div_img" style="background-image: url(data:image/jpeg;base64,<?php echo($header_image); ?>);"> </div>
+            <div id="top_div_img" style="background-image: url('data:image/jpeg;base64,<?php echo($header_image); ?>');"> </div>
             <div id="item_desc">
-                <h1><?php echo $title ?></h1>
-                <p class="item_desc_text">consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes</p>
-                <button class="order_button"><?php echo($order_button_text) ?></button>
-                <button class="order_button">Read more</button>
+                <h1><?php echo($title); ?></h1>
+                <p class="item_desc_text"><?php echo($desc_text); ?></p>
+                <button class="order_button"><?php echo($order_button); ?></button>
             </div>
             <div class="v-align-helper"></div>
         </div>
-
-
-
 
         <!--   Information about the 3 main features of product     -->
         <div id="info_div_container">
             <div class="v-align-helper"></div>
 
-            <h2>Features</h2>
+            <h2><?php echo($top_small_title); ?></h2>
 
             <div id="info_div">
             <div id="info_div1" class="info_box">
                 <img id="info_div_img1" src="http://placehold.it/300x150">
-                <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, </p>
+                <p><?php echo($large_desc_1); ?></p>
             </div>
 
             <div id="info_div2" class="info_box">
                 <img id="info_div_img2" src="http://placehold.it/300x150">
-                <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+                <p><?php echo($large_desc_2); ?></p>
             </div>
 
             <div id="info_div3" class="info_box">
                 <img id="info_div_img3" src="http://placehold.it/300x150">
-                <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo</p>
+                <p><?php echo($large_desc_3); ?></p>
             </div>
-
         </div>
         </div>
-
-        <!--  Variations   -->
-        <div id="products_div_container">
-            <div class="v-align-helper"></div>
-
-            <h2>Product</h2>
-
-            <div id="products_div">
-                <div id="product_box1"  class="products_box">
-                    <img id="products_div_img1" src="http://placehold.it/150x150">
-                    <p class="product_small_desc">L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-                    <button class="item_order_button">1m $9,99</button>
-                </div>
-
-                <div id="products_box2" class="products_box">
-                    <img id="products_div_img3" src="http://placehold.it/150x150">
-                    <p class="product_small_desc">L Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                    <button class="item_order_button">2m $18,99</button>
-                </div>
-
-                <div id="products_box3" class="products_box">
-                    <img id="products_div_img3" src="http://placehold.it/150x150">
-                    <p class="product_small_desc"><?php echo $small_description ?></p>
-                    <button class="item_order_button">5m $39,99</button>
-                </div>
-
-                <div id="products_box4" class="products_box">
-                    <img id="products_div_img4" src="http://placehold.it/150x150">
-                    <p class="product_small_desc">L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                    <button class="item_order_button">10m $79,99</button>
-                </div>
-
-            </div>
-            <div class="dropdown">
-                    <button class="dropbtn">Choose length</button>
-                    <div class="dropdown-content">
-                        <a href="#">1m $9,99</a>
-                        <a href="#">2m $18,99</a>
-                        <a href="#">5m $39,99</a>
-                        <a href="#">10m $79,99</a>
-                    </div>
-            </div>
-
 
         <!--    information about delivery: order > deliver > time/wait    -->
         <div id="order_div_container">
             <div class="v-align-helper"></div>
 
-            <h2>Delivery</h2>
+            <h2><?php echo($bottom_small_title); ?></h2>
 
             <div id="order_div">
                 <div id="order_box1"  class="order_box">
                     <img id="order_div_img1" src="http://placehold.it/150x150">
-                    <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                    <p><?php echo($small_desc_1); ?></p>
                 </div>
 
                 <div id="order_box2" class="order_box">
                     <img id="order_div_img3" src="http://placehold.it/150x150">
-                    <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </p>
+                    <p><?php echo($small_desc_2); ?></p>
                 </div>
 
                 <div id="order_box3" class="order_box">
                     <img id="order_div_img3" src="http://placehold.it/150x150">
-                    <p>L Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+                    <p><?php echo($small_desc_3); ?></p>
                 </div>
+            </div>
+
+            <div id="order_button_big_remind">
+                <button class="order_button_big"><?php echo($order_button); ?></button>
             </div>
         </div>
 
@@ -145,7 +99,5 @@
             </div>
 
         </div>
-        </div>
 	</body>
 </html>
-
