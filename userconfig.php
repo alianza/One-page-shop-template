a76f6f2eb23a2aeeae179241838522fd6052c43c
PhyrexAlianza <janwillemvanbremen@live.nl>
2017-09-03 19:36:52 +0200

    New file with Id's and credentials

---
+++ userconfig.php
@@ -0,0 +1,15 @@
+<?
+
+    //Only the next 4 lines of code should ever be changed by a non-developer
+
+    //Onepage ID for text and images
+    $onepage_ID = 1;
+
+    //Global ID for Footer links and images
+    $global_ID = 1;
+
+    //database user credentials
+    DEFINE("DB_USER", "database_admin");
+    DEFINE  ("DB_PASS", "DamsCommerce12");
+
+?>
