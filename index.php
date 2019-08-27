b0d21fe675a06f37c8da94d98f9239d80f54c4ee
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2019-08-27 21:00:10 +0200

    Favicons added

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
     <meta name="viewport" content="initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href=".//css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+<!--    Favicon Generator-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#c1282d">
+    <meta name="msapplication-TileColor" content="#da532c">
+<!--    End Favicon Generator-->
+    <meta name="theme-color" content="#c1282d">
     <?php include("dbconfig.php") ?>
     <title><?php echo($title); ?></title>
 </head>
